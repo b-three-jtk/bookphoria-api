c4b6fd265843dc63e91f5a7b40c0fdb552b5b285
Alanna Tanisya Anwar <tanisyaalanna@gmail.com>
2025-06-16 06:31:51 +0700

    fix : update breadcumb for index user

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,5 +3,12 @@
 @section('title', 'User Management')
 
 @section('content')
-    
+    <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
+        <!-- Breadcrumb Start -->
+        <div x-data="{ pageName: `Basic Tables` }">
+            <include src="./partials/breadcrumb.html" />
+        </div>
+        <!-- Breadcrumb End -->
+         
+    </div>
 @endsection
