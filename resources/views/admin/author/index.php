ddde1124aa3aff3ab50cce889e68c1e1f370f306
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2025-06-16 05:41:09 +0700

    feat(admin): set up the basic Blade template structure with layout extension, title, and main container

---
+++ resources/views/admin/author/index.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title', 'User Management')
+@section('content')
+    <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
+        <div class="space-y-5 sm:space-y-6">
+        </div>
+    </div>
+@endsection
