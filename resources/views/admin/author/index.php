cb9b98ba99db3635c47bfe387a557d3abd7a502e
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2025-06-16 05:42:24 +0700

    feat(admin): add the "Data Penulis" section with header and basic table structure (without table body)

--- resources/views/admin/author/index.php
+++ resources/views/admin/author/index.php
@@ -8,6 +8,66 @@
         </div>
         <!-- Breadcrumb End -->
         <div class="space-y-5 sm:space-y-6">
+            <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                <div class="px-5 py-4 sm:px-6 sm:py-5">
+                    <h3 class="text-base font-medium text-gray-800 dark:text-white/90">
+                        Data Penulis
+                    </h3>
+                </div>
+                <div class="p-5 border-t border-gray-100 dark:border-gray-800 sm:p-6">
+                    <div
+                        class="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                        <div class="max-w-full overflow-x-auto">
+                            <table class="min-w-full">
+                                <!-- table header start -->
+                                <thead>
+                                    <tr class="border-b border-gray-100 dark:border-gray-800">
+                                        <th class="px-5 py-3 sm:px-6">
+                                            <div class="flex items-center">
+                                                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">
+                                                    No
+                                                </p>
+                                            </div>
+                                        </th>
+                                        <th class="px-5 py-3 sm:px-6">
+                                            <div class="flex items-center">
+                                                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">
+                                                    Nama Penulis
+                                                </p>
+                                            </div>
+                                        </th>
+                                        <th class="px-5 py-3 sm:px-6">
+                                            <div class="flex items-center">
+                                                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">
+                                                    Jumlah Buku
+                                                </p>
+                                            </div>
+                                        </th>
+                                        <th class="px-5 py-3 sm:px-6">
+                                            <div class="flex items-center">
+                                                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">
+                                                    Tanggal Dibuat
+                                                </p>
+                                            </div>
+                                        </th>
+                                        <th class="px-5 py-3 sm:px-6">
+                                            <div class="flex items-center">
+                                                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">
+                                                    Aksi
+                                                </p>
+                                            </div>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <!-- table header end -->
+                                <!-- table body start -->
+                                <tbody class="divide-y divide-gray-100 dark:divide-gray-800">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
