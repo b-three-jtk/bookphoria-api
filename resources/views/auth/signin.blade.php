b4e442a232ac9d3cc9ddb5dc617b8197db247f03
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2025-06-16 05:29:46 +0700

    feat(signin): add form for signin

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -5,7 +5,19 @@
 <div class="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
     <div>
         <div>
-            <div>    
+            <!-- Form -->
+            <div class="flex flex-col flex-1 w-full lg:w-1/2">
+            <div class="w-full max-w-md pt-10 mx-auto">
+                <a href="{{ route('welcome') }}"
+                    class="inline-flex items-center text-sm text-gray-500 transition-colors hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
+                    <svg class="stroke-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 20 20" fill="none">
+                        <path d="M12.7083 5L7.5 10.2083L12.7083 15.4167" stroke="" stroke-width="1.5"
+                            stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    Back to landing page
+                </a>
+            </div>    
                 <div>                 
                     <div>            
                         <div>
