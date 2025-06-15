cb25826f2deaba8c5600d3dd5c1c28260c725145
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2025-06-16 05:34:43 +0700

    feat(signup): initialize signup page with blade template structure

---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.blank')
+@section('title', 'Sign Up')
+@section('content')
+    <div class="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
+        <div class="flex flex-col justify-center w-full h-screen dark:bg-gray-900 sm:p-0 lg:flex-row">
+            <!-- Form -->
+            <div class="flex flex-col flex-1 w-full lg:w-1/2">
+                <div class="w-full max-w-md pt-5 mx-auto sm:py-10">
+                </div>
+                <div class="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
+                </div>
+            </div>
+            <div class="relative items-center hidden w-full h-full bg-brand-950 dark:bg-white/5 lg:grid lg:w-1/2">
+            </div>
+        </div>
+    </div>
+@endsection
