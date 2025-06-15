082afce4c76af37437e09c464364a6dfed354112
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2025-06-16 05:41:43 +0700

    feat(admin): add the breadcrumb component

--- resources/views/admin/author/index.php
+++ resources/views/admin/author/index.php
@@ -2,6 +2,11 @@
 @section('title', 'User Management')
 @section('content')
     <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
+        <!-- Breadcrumb Start -->
+        <div x-data="{ pageName: `Basic Tables` }">
+            <include src="./partials/breadcrumb.html" />
+        </div>
+        <!-- Breadcrumb End -->
         <div class="space-y-5 sm:space-y-6">
         </div>
     </div>
